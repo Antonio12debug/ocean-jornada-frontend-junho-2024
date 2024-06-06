966b013d600d7184556b28d17929f6be88ddbdaa
Antonio12debug <95870195+Antonio12debug@users.noreply.github.com>
2024-06-06 22:52:28 +0000

    Estrutura HTML criada

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App(){
   return(
     <>
     <div className='card'>
-      <h2>ick Sanchez</h2>
+      <h2>Rick Sanchez</h2>
       <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
     </div>
     </>
